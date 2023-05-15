1434b4c726791c5c9590ab92dcf2266d586b4a6f
FR4NC1SC011 <fcootzg@gmail.com>
2023-05-15 16:37:37 -0600

    Agregamos algunos comentarios

--- nerf.py
+++ nerf.py
@@ -25,9 +25,9 @@ def test(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400)
     ray_origins = dataset[img_index * H * W: (img_index + 1) * H * W, :3]
     ray_directions = dataset[img_index * H * W: (img_index + 1) * H * W, 3:6]
 
-    data = []   # list of regenerated pixel values
-    for i in range(int(np.ceil(H / chunk_size))):   # iterate over chunks
-        # Get chunk of rays
+    data = []   # Lista de valores de los pixeles regenerados
+    for i in range(int(np.ceil(H / chunk_size))):   # Iteramos sobre los chunks
+        # Obtenemos chunks de rayos
         ray_origins_ = ray_origins[i * W * chunk_size: (i + 1) * W * chunk_size].to(device)
         ray_directions_ = ray_directions[i * W * chunk_size: (i + 1) * W * chunk_size].to(device)        
         regenerated_px_values = render_rays(model, ray_origins_, ray_directions_, hn=hn, hf=hf, nb_bins=nb_bins)
@@ -48,12 +48,12 @@ class NerfModel(nn.Module):
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), )
-        # density estimation
+        # estimacion de densidad
         self.block2 = nn.Sequential(nn.Linear(embedding_dim_pos * 6 + hidden_dim + 3, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim + 1), )
-        # color estimation
+        # estimacion de color
         self.block3 = nn.Sequential(nn.Linear(embedding_dim_direction * 6 + hidden_dim + 3, hidden_dim // 2), nn.ReLU(), )
         self.block4 = nn.Sequential(nn.Linear(hidden_dim // 2, 3), nn.Sigmoid(), )
 
@@ -70,13 +70,13 @@ class NerfModel(nn.Module):
         return torch.cat(out, dim=1)
 
     def forward(self, o, d):
-        emb_x = self.positional_encoding(o, self.embedding_dim_pos) # emb_x: [batch_size, embedding_dim_pos * 6]
-        emb_d = self.positional_encoding(d, self.embedding_dim_direction) # emb_d: [batch_size, embedding_dim_direction * 6]
-        h = self.block1(emb_x) # h: [batch_size, hidden_dim]
-        tmp = self.block2(torch.cat((h, emb_x), dim=1)) # tmp: [batch_size, hidden_dim + 1]
-        h, sigma = tmp[:, :-1], self.relu(tmp[:, -1]) # h: [batch_size, hidden_dim], sigma: [batch_size]
-        h = self.block3(torch.cat((h, emb_d), dim=1)) # h: [batch_size, hidden_dim // 2]
-        c = self.block4(h) # c: [batch_size, 3]
+        emb_x = self.positional_encoding(o, self.embedding_dim_pos) 
+        emb_d = self.positional_encoding(d, self.embedding_dim_direction) 
+        h = self.block1(emb_x)
+        tmp = self.block2(torch.cat((h, emb_x), dim=1)) 
+        h, sigma = tmp[:, :-1], self.relu(tmp[:, -1]) 
+        h = self.block3(torch.cat((h, emb_d), dim=1)) 
+        c = self.block4(h)
         return c, sigma
 
 
@@ -90,28 +90,26 @@ def render_rays(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=1
     device = ray_origins.device
     
     t = torch.linspace(hn, hf, nb_bins, device=device).expand(ray_origins.shape[0], nb_bins)
-    # Perturb sampling along each ray.
     mid = (t[:, :-1] + t[:, 1:]) / 2.
     lower = torch.cat((t[:, :1], mid), -1)
     upper = torch.cat((mid, t[:, -1:]), -1)
     u = torch.rand(t.shape, device=device)
-    t = lower + (upper - lower) * u  # [batch_size, nb_bins]
+    t = lower + (upper - lower) * u 
     delta = torch.cat((t[:, 1:] - t[:, :-1], torch.tensor([1e10], device=device).expand(ray_origins.shape[0], 1)), -1)
 
-    # Compute the 3D points along each ray
-    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(1)   # [batch_size, nb_bins, 3]
-    # Expand the ray_directions tensor to match the shape of x
+    # Calculamos los puntos 3D de cada rayo
+    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(1) 
     ray_directions = ray_directions.expand(nb_bins, ray_directions.shape[0], 3).transpose(0, 1) 
 
     colors, sigma = nerf_model(x.reshape(-1, 3), ray_directions.reshape(-1, 3))
     colors = colors.reshape(x.shape)
     sigma = sigma.reshape(x.shape[:-1])
 
-    alpha = 1 - torch.exp(-sigma * delta)  # [batch_size, nb_bins]
+    alpha = 1 - torch.exp(-sigma * delta) 
     weights = compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(2)
-    # Compute the pixel values as a weighted sum of colors along each ray
+    # Calculamos los valores de los pixeles como la suma de colores en cada rayo
     c = (weights * colors).sum(dim=1)
-    weight_sum = weights.sum(-1).sum(-1)  # Regularization for white background 
+    weight_sum = weights.sum(-1).sum(-1)  # Regularizamos para fondo blanco
     return c + 1 - weight_sum.unsqueeze(-1)
 
 
